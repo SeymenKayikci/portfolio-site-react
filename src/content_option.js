71f6a05ec1bca9c49d5fd075c7fee96147a61eda
SeymenKayikci <138564064+SeymenKayikci@users.noreply.github.com>
2024-09-20 15:55:12 +0300

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "Seymen";
+const meta = {
+    title: "Seymen KAYIKÇI",
+    description: "Ben Seymen Bilgisayar Programcısıyım, İzmir'de Yaşıyorum.",
+};
+
+const introdata = {
+    title: "Ben Seymen KAYIKÇI",
+    animated: {
+        first: "Kod yazmak benim işim.",
+        second: "Web sitesi tasarlıyorum.",
+        third: "Yapay Zeka da ilgi alanlarımdan bir tanesi.",
+    },
+    description: "Hakkımda Biraz Daha Detaylı Bilgi İçin Sitemi İncelemeye Devam Edin.",
+    your_img_url: "https://media.licdn.com/dms/image/D4E03AQFD4MPm5LOS7Q/profile-displayphoto-shrink_800_800/0/1695728317881?e=1722470400&v=beta&t=7Y3ynBHVO5caRdSFAyiK7DwAhViqkzpviPeCgQTy_bs",
+};
+
+const dataabout = {
+    title: "Ben Kimim ?",
+    aboutme: "22.04.2002 İzmir-Konak ilçesinde doğdum. Lise öğrenimimi Bornova Final Anadolu-Fen Lisesi'nde tamamladım. İzmir Ekonomi Üniversitesi Bilgisayar Programcılığı Bölümü'nde son sınıf öğrencisiyim.Stajımı Üniteks Firması'nın IT departmanında tamamladım. İş hayatıma yaptığım başarılı web sitelerinden sonra referans aracılığı ile Kum Travel Web Developer olarak freelance çalışmaktayım.",
+};
+const worktimeline = [{
+        jobtitle: "Üniteks Tekstil",
+        where: "İzmir/Çiğli",
+        date: "2023",
+    },
+    {
+        jobtitle: "KumTravel",
+        where: "Marmaris",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 40,
+    },
+    {
+        name: "C#",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Web Sitesi Tasarımı",
+        description: "React-JavaScript-Html-Css kullanarak çeşitli web siteleri üzerinde çalıştım.",
+    },
+    {
+        title: "Turizm Seyehat Aplikasyonu",
+        description: "Pyhton kullanarak çeşitli uygulama tasarımları üzerinde çalıştım.",
+    },
+    {
+        title: "Otopark Otomasyonu",
+        description: "C# form kullanarak otomasyonlar tasarladım",
+    },
+    {
+        title: "Veri Analizi ve Makine Öğrenmesi",
+        description: "Numpy, Pandas, Seaborn, Matplotlip ve Sklearn kullanarak veri analizi ve makine öğrenmesi kullanarak verisetlerini inceledim.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "seymennkayikci@gmail.com",
+    YOUR_FONE: "(535)934-8204",
+    description: "Görüş ve Önerileriniz için. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
